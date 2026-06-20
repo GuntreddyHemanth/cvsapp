d0d7a4587d4f6653f2f1c81bf127343c532e3f0e
stemworld739-del <stemworld739@gmail.com>
2026-06-20 18:03:19 +0530

    feat: complete 3-box registration flow and branding updates

--- vector/src/main/java/im/vector/app/features/onboarding/ftueauth/FtueAuthChooseDisplayNameFragment.kt
+++ vector/src/main/java/im/vector/app/features/onboarding/ftueauth/FtueAuthChooseDisplayNameFragment.kt
@@ -1,21 +1,10 @@
-/*
- * Copyright 2021-2024 New Vector Ltd.
- *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
- * Please see LICENSE files in the repository root for full details.
- */
-
 package im.vector.app.features.onboarding.ftueauth
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import dagger.hilt.android.AndroidEntryPoint
-import im.vector.app.core.extensions.hasContent
-import im.vector.app.core.platform.SimpleTextWatcher
 import im.vector.app.databinding.FragmentFtueDisplayNameBinding
 import im.vector.app.features.onboarding.OnboardingAction
 import im.vector.app.features.onboarding.OnboardingViewEvents
@@ -31,43 +20,18 @@ class FtueAuthChooseDisplayNameFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupViews()
-    }
-
-    private fun setupViews() {
-        views.displayNameInput.editText?.addTextChangedListener(object : SimpleTextWatcher() {
-            override fun afterTextChanged(s: Editable) {
-                val newContent = s.toString()
-                views.displayNameSubmit.isEnabled = newContent.isNotEmpty()
-            }
-        })
-        views.displayNameInput.editText?.setOnEditorActionListener { _, actionId, _ ->
-            when (actionId) {
-                EditorInfo.IME_ACTION_DONE -> {
-                    updateDisplayName()
-                    true
-                }
-                else -> false
-            }
+        views.displayNameSubmit.debouncedClicks {
+            val name = views.displayNameInput.editText?.text.toString()
+            viewModel.handle(OnboardingAction.UpdateDisplayName(name))
         }
-
-        views.displayNameSubmit.debouncedClicks { updateDisplayName() }
         views.displayNameSkip.debouncedClicks { viewModel.handle(OnboardingAction.UpdateDisplayNameSkipped) }
     }
 
-    private fun updateDisplayName() {
-        val newDisplayName = views.displayNameInput.editText?.text.toString()
-        viewModel.handle(OnboardingAction.UpdateDisplayName(newDisplayName))
-    }
-
     override fun updateWithState(state: OnboardingViewState) {
-        views.displayNameInput.editText?.setText(state.personalizationState.displayName)
-        views.displayNameSubmit.isEnabled = views.displayNameInput.hasContent()
+        views.displayNameSubmit.isEnabled = !views.displayNameInput.editText?.text.isNullOrEmpty()
     }
 
-    override fun resetViewModel() {
-        // Nothing to do
-    }
+    override fun resetViewModel() {}
 
     override fun onBackPressed(toolbarButton: Boolean): Boolean {
         viewModel.handle(OnboardingAction.PostViewEvent(OnboardingViewEvents.OnTakeMeHome))
